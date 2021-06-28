f690f8110524e720c13893b395153c30281cab26
mallgrab <mallgrab@github.com>
2021-06-28 22:46:18 +0200

    Read header from mesh

--- Rblx2Blender/rblxmesh.py
+++ Rblx2Blender/rblxmesh.py
@@ -1,13 +1,30 @@
 from io import BufferedReader
 from array import array
+import struct
 
-path = "/home/user/Desktop/RobloxMeshLoader/RobloxMeshLoader/MeshTesting_V3"
+path = "./meshes/MeshTesting_V3"
 
 def MeshReader(file: BufferedReader):
-    file.read(7)
+    file.read(1)
+    mesh_version = float(file.read(4))
+    file.read(1)
+    file.read(2) # padding?
+    vertex_size = int.from_bytes(file.read(1), "little") 
+    file.read(1) # padding?
+    file.read(2) # padding?
+    num_lods = int.from_bytes(file.read(2), "little")
+    num_verts = int.from_bytes(file.read(4), "little")
+    num_faces = int.from_bytes(file.read(4), "little")
 
-with open(path, "rb") as f:
-    version_string = f.read(7)
+    pos_x = struct.unpack('<f', file.read(4))
+    pos_y = struct.unpack('<f', file.read(4))
+    pos_z = struct.unpack('<f', file.read(4))
+    print(pos_x, pos_y, pos_z)
 
-    if (version_string.decode("utf-8") == 'version'):
-        MeshReader(f)
+def OpenMeshFile(path):
+    with open(path, "rb") as file:
+        version_string = file.read(7)
+        if (version_string.decode("utf-8") == 'version'):
+            MeshReader(file)
+
+OpenMeshFile(path)
