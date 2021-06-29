643b2d63987539abbe29b5b213a95a9e21ffc900
mallgrab <mallgrab@github.com>
2021-06-29 04:09:28 +0200

    fill vertex_list

--- Rblx2Blender/rblxmesh.py
+++ Rblx2Blender/rblxmesh.py
@@ -15,12 +15,20 @@ class Vector2():
         self.x = x
         self.y = y
 
+class Color_ARGB():
+    def __init__(self, A: int, R: int, G: int, B: int):
+        self.A = A
+        self.R = R
+        self.G = G
+        self.B = B
+
 class Vertex():
-    def __init__(self, position: Vector3, normals: Vector3, uv: Vector2, vertex_color: int):
+    def __init__(self, position: Vector3, normals: Vector3, uv: Vector2, vertex_color: Color_ARGB):
         self.position = position
         self.normals = normals
         self.uv = uv
         self.vertex_color = vertex_color
+
 
 def Vector3Float(file: BufferedReader):
     x = struct.unpack('<f', file.read(4))
@@ -28,6 +36,15 @@ def Vector3Float(file: BufferedReader):
     z = struct.unpack('<f', file.read(4))
 
     return Vector3(x, y, z)
+
+def VertexColor(vertex_color: int):
+    vertex_bytes = vertex_color.to_bytes(4, 'little')
+
+    a = vertex_bytes[0]
+    r = vertex_bytes[1]
+    g = vertex_bytes[2]
+    b = vertex_bytes[3]
+    return Color_ARGB(a, r, g, b)
 
 def MeshReader(file: BufferedReader):
     file.read(1)
@@ -41,15 +58,20 @@ def MeshReader(file: BufferedReader):
     num_verts = int.from_bytes(file.read(4), "little")
     num_faces = int.from_bytes(file.read(4), "little")
 
-    position = Vector3Float(file)
-    normals = Vector3Float(file)
-    
-    uv_tmp = Vector3Float(file)
-    uv = Vector2(uv_tmp.x, uv_tmp.y)
+    vertex_list = []
+    for i in range(num_verts):
+        position = Vector3Float(file)
+        normals = Vector3Float(file)
 
-    print(position.x, position.y, position.z)
-    print(normals.x, normals.y, normals.z)
-    print(uv.x, uv.y)
+        uv_tmp = Vector3Float(file)
+        uv = Vector2(uv_tmp.x, uv_tmp.y)
+
+        color_argb = int.from_bytes(file.read(4), "little")
+        vertex_color = VertexColor(color_argb)
+
+        vertex_list.append(Vertex(position, normals, uv, vertex_color))
+
+    print("done")
 
 def OpenMeshFile(path):
     with open(path, "rb") as file:
