e1be56948d0e1792512764107b2e05147023f43b
mallgrab <mallgrab@github.com>
2021-07-23 04:56:23 +0200

    type annotation fix

--- Rbx2Blender/rbx/convert.py
+++ Rbx2Blender/rbx/convert.py
@@ -9,7 +9,7 @@ from typing import List
 from . legacycolors import BrickColor
 from . assetreader import MeshAsset
 from . assetrequester import AssetRequester
-from . types import Part, TileUV, Brick, Texture
+from . types import Part, TileUV, Brick, Texture, TextureMd5
 
 from . assetrequester import AssetRequester
 from . mesh import GetMeshFromMeshData
