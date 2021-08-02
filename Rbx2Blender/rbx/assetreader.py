8a4682a596dca3634df350221dab2eedc0a52788
mallgrab <mallgrab@github.com>
2021-08-02 04:08:09 +0200

    Change MeshAssetContent & Ids to HatAsset since only GetMeshFromHat uses it.

--- Rbx2Blender/rbx/assetreader.py
+++ Rbx2Blender/rbx/assetreader.py
@@ -1,22 +1,27 @@
 import io
 import xml.etree.ElementTree as ET
 
-class MeshAssetIds(object):
+class HatAssetIds(object):
     def __init__(self, mesh, texture):
         self.mesh = mesh
         self.texture = texture
 
-class MeshAssetContent(object):
+class HatAssetContent(object):
     def __init__(self, mesh, texture):
         self.mesh = mesh
         self.texture = texture
 
-class MeshAsset(object):
-    def __init__(self, content: MeshAssetContent, ids: MeshAssetIds):
+class HatAsset(object):
+    def __init__(self, content: HatAssetContent, ids: HatAssetIds):
         self.content = content
         self.ids = ids
 
-def XMLAssetReader(file: str):
+class MeshAsset(object):
+    def __init__(self, mesh_content, mesh_id):
+        self.mesh_content = mesh_content
+        self.mesh_id = mesh_id
+
+def HatXMLAssetReader(file: str):
     root = ET.parse(io.StringIO(file)).getroot()
     mesh_url = root.findall(".//*[@class='SpecialMesh']/Properties/Content[@name='MeshId']/url")[0].text
     texture_url = root.findall(".//*[@class='SpecialMesh']/Properties/Content[@name='TextureId']/url")[0].text
@@ -25,15 +30,15 @@ def XMLAssetReader(file: str):
     mesh_id = mesh_url[mesh_url.find('='):][1:]
     texture_id = texture_url[texture_url.find('='):][1:]
 
-    mesh_asset_ids = MeshAssetIds(mesh_id, texture_id)
+    mesh_asset_ids = HatAssetIds(mesh_id, texture_id)
     return mesh_asset_ids
 
-def BinaryAssetReader(file: bytes):
+def HatBinaryAssetReader(file: bytes):
     _ = file[file.find(b"MeshId"):][24:]
     mesh_id = _[:_.find(b"PROP")].decode("ascii")
 
     _ = file[file.find(b"TextureId"):][27:]
     texture_id = _[:_.find(b"PROP")].decode("ascii")
 
-    mesh_asset_ids = MeshAssetIds(mesh_id, texture_id)
+    mesh_asset_ids = HatAssetIds(mesh_id, texture_id)
     return mesh_asset_ids
