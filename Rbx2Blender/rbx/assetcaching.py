5fe35f1b173db11bfd195715d635fec44f31fd16
mallgrab <mallgrab@github.com>
2021-08-08 05:33:25 +0200

    remove duplicates from asset caching list

--- Rbx2Blender/rbx/assetcaching.py
+++ Rbx2Blender/rbx/assetcaching.py
@@ -3,5 +3,5 @@ class AssetCaching(object):
 
     @staticmethod
     def PrefetchAssets():
-        for asset_id in AssetCaching.asset_ids:
-            print(asset_id)
+        AssetCaching.asset_ids = list(set(AssetCaching.asset_ids))
+        _v = AssetCaching.asset_ids
