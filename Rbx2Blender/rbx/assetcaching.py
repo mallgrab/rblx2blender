ee1387bf24bff4cc91c709a80cf6d30b0ef3dfae
mallgrab <mallgrab@github.com>
2021-08-20 05:06:10 +0200

    comments regarding the caching procedure

--- Rbx2Blender/rbx/assetcaching.py
+++ Rbx2Blender/rbx/assetcaching.py
@@ -25,31 +25,31 @@ class AssetCaching(object):
         AssetCaching.assets = list(set(AssetCaching.assets))
         cached_asset_ids = [os.path.basename(asset) for asset in glob.glob(AssetRequester.asset_dir + "/*")]
 
-        # remove binary textures
+        # binary textures are already cached by their md5 sum
         for index, asset_id in enumerate(cached_asset_ids):
             if asset_id.find("tex_") > -1:
                 cached_asset_ids.pop(index)
 
-        # remove extension of local asset
+        # remove extension from local asset's name
         for index, asset_id in enumerate(cached_asset_ids):
             asset_id_split = asset_id.split(".")
             if asset_id_split:
                 cached_asset_ids[index] = asset_id_split[0]
 
-
+        # add assets that are already cached
         cached_asset_set = []
         for asset in asset_set:
             for asset_id in cached_asset_ids:
                 if asset.asset_id == asset_id:
                     cached_asset_set.append(asset)
         
+        # remove assets that exist in both lists
         uncached_assets = list(reduce(lambda x,y : filter(lambda z: z!=y,x), cached_asset_set, asset_set))
 
+        # do requests on the assets so that they get saved locally and are then cached
         asset: Asset
         for asset in uncached_assets:
             if asset.asset_type == "mesh":
                 AssetRequester.GetAssetFromId(asset.asset_id)
             if asset.asset_type == "texture":
                 AssetRequester.GetOnlineTexture(asset.asset_id)
-        
-        _v = uncached_assets
